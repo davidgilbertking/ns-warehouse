55a2cfbe63c31ccbc6357938734602f15226357e
David Kazaryan <david_kazaryan@mail.ru>
2025-04-29 11:42:26 +0300

    test final fixes

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,19 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase; // <<< добавить это
+
     public function test_the_application_returns_a_successful_response(): void
     {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
