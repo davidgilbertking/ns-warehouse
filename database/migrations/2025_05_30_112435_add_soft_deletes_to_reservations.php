d7372cf0036818bd375e5d60630713917634347b
David Kazaryan <david_kazaryan@mail.ru>
2025-05-30 15:29:16 +0300

    drop soft deletes in migration

--- database/migrations/2025_05_30_112435_add_soft_deletes_to_reservations.php
+++ database/migrations/2025_05_30_112435_add_soft_deletes_to_reservations.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('reservations', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
